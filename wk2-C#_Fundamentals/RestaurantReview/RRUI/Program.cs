1009f6d9d46f33e56343789274dbe4cbdd50038e
Scifiler-sys <pagdilaostephen@gmail.com>
2021-10-07 11:20:31 -0400

    Added comments

--- wk2-C#_Fundamentals/RestaurantReview/RRUI/Program.cs
+++ wk2-C#_Fundamentals/RestaurantReview/RRUI/Program.cs
@@ -6,27 +6,41 @@ namespace RRUI
     {
         static void Main(string[] args)
         {
+            //This is a boolean that has either a false or true value
+            //I will use this to stop the while loop
             bool repeat = true;
+
+
             //This is example of polymorphism, abstraction, and inheritance all at the same time
             IMenu page = new MainMenu();
 
+            //This is a while loop that will keep repeating until I changed the
+            //repeat variable to false
             while (repeat)
             {
                 //Clean the screen on the terminal
                 Console.Clear();
-                
+
+                //IMenu interface can hold a bunch of objects as long as they inherited from
+                //IMenu, this lets us dynamically change the page by having the page variable
+                //Point to a different object each time
                 page.Menu();
                 MenuType currentPage = page.YourChoice();
 
+                //switch case will change the page variable to point to another object to change
+                //its MainMenu 
                 switch (currentPage)
                 {
                     case MenuType.MainMenu:
+                        //If user choosed to go back to the MainMenu
+                        //page will start pointing to a MainMenu object instead
                         page = new MainMenu();
                         break;
                     case MenuType.RestaurantMenu:
                         //This will point the page reference variable to a new Restaurant Object
                         //Since Restaurant Object has different implementation/function of the Menu Method
-                        //It will change my terminal to reflect that change
+                        //It will have different implementations/functions when the while loop goes back and
+                        //repeat itself
                         page = new RestaurantMenu();
                         break; 
                     case MenuType.Exit:
