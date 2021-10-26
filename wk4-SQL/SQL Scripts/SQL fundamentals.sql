66cf0e31784cf152a76005fa4919c8b7f5f3072a
Scifiler-sys <pagdilaostephen@gmail.com>
2021-10-26 11:54:27 -0400

    Added More multiplicity and constraints

--- wk4-SQL/SQL Scripts/SQL fundamentals.sql
+++ wk4-SQL/SQL Scripts/SQL fundamentals.sql
@@ -8,6 +8,7 @@ create table avenger(
 	power_level int
 );
 
+
 --Alter will change the table either add column, constraints or remove them
 alter table avenger add dead bit;
 
@@ -34,10 +35,18 @@ where avenger_name = 'Capt. America'
 insert into avenger(avenger_name, avenger_power,real_name , power_level)
 	values('Capt. America', 'Drugs', 'Steve Rogers', 200),
 		('Ironman', 'Money', 'Tony Stark', 9999)
-
+		
 ------------- Multiplicity -------------
 
 --One to One Relationship	
+create table heart(
+	heart_size int,
+	heart_healthy bit,
+	person_SSN int unique foreign key references person(SSN) --This will automatically fill in some auto generate named for the foreign key
+)
+
+insert into heart(heart_size, heart_healthy, person_SSN)
+	values (50, 0, 2) --Cannot add another heart to Colin which has an SSN of 2
 
 --One to Many Relationship
 create table person
@@ -68,6 +77,45 @@ insert into person(SSN, person_name, person_age)
 insert into fingers(finger_length, finger_type, person_SSN)
 	values(3, 'Pointy finger', 2),
 		(1, 'Pinky finger', 1)
+
+--Many to Many relationship
+create table class(
+	class_id int primary key,
+	class_name varchar(30)
+)
+
+insert into class(class_id, class_name)
+	values (1, '.NET course'),
+			(2, 'Intro to Psychology'),
+			(3, 'Home economics')
+
+-- Join table of person and class
+create table person_class(
+	person_SSN int foreign key references person(SSN),
+	class_id int foreign key references class(class_id)
+)
+
+--We inserted value that will always refernce to another row in both person and class table
+insert into person_class (person_SSN, class_id)
+	values (2, 1), -- 2 SSN which is Colin will have 1 class id which is the .NET course
+			(1, 1), -- Since Adam will also point to 1 class id which is .NET, both Colin and Adam will have the .NET course
+			(1, 2), -- 1 SSN which is Adam will have multiple class id to indicate that person will have multiple classes
+			(1, 3)
+
+-- This inner join will point to how many students are currently taking the .NET course
+select p.person_name, c.class_name from person p
+inner join person_class pc on p.SSN = pc.person_SSN 
+inner join class c on c.class_id = pc.class_id 
+where c.class_id = 1 -- Just change the where clause to limit what type of data you are looking for
+
+-- This inner join will poin to how many courses Adam is currently taking 
+select p.person_name, c.class_name from person p
+inner join person_class pc on p.SSN = pc.person_SSN 
+inner join class c on c.class_id = pc.class_id 
+where p.SSN = 1 -- In this case, I'm looking for a person_SSN instead
+
+-- Overall these inner joins just select data from the many to many relationship into one to many relationship that SQL understands
+
 
 --Quick example of inner joins
 select p.person_name, f.finger_type from person p
