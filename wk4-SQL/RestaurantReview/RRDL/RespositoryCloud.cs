cc49d6b29381b9c469b7632a7cd99fa3ffd91178
Scifiler-sys <pagdilaostephen@gmail.com>
2021-10-27 13:52:59 -0400

    Added method syntax for GetAllReviews

--- wk4-SQL/RestaurantReview/RRDL/RespositoryCloud.cs
+++ wk4-SQL/RestaurantReview/RRDL/RespositoryCloud.cs
@@ -87,22 +87,32 @@ namespace RRDL
         public List<Model.Review> GetAllReview(Model.Restaurant p_rest)
         {
             //Query syntax
-            var result = (from rev in _context.Reviews
-                        where rev.RestId == p_rest.Id
-                        select rev);
+            // var result = (from rev in _context.Reviews
+            //             where rev.RestId == p_rest.Id
+            //             select rev);
 
-            //Mapping the Queryable<Entity.Review> into a list<Model.Review>
-            List<Model.Review> listOfReview = new List<Model.Review>();
-            foreach (Entity.Review rev in result)
-            {
-                listOfReview.Add(new Model.Review(){
-                    Id = rev.RevId,
-                    Rating = rev.RevRating,
-                    RestId = rev.RestId
-                });
-            }
+            // //Mapping the Queryable<Entity.Review> into a list<Model.Review>
+            // List<Model.Review> listOfReview = new List<Model.Review>();
+            // foreach (Entity.Review rev in result)
+            // {
+            //     listOfReview.Add(new Model.Review(){
+            //         Id = rev.RevId,
+            //         Rating = rev.RevRating,
+            //         RestId = rev.RestId
+            //     });
+            // }
 
-            return listOfReview;
+            // return listOfReview;
+
+            //Method Syntax - since this looks cleaner
+            return _context.Reviews
+                .Where(rev => rev.RevId == p_rest.Id)
+                .Select(rev => new Model.Review(){
+                  Id = rev.RevId,
+                  Rating = rev.RevRating,
+                  Restaurant = p_rest
+                })
+                .ToList();
         }
     }
 }
