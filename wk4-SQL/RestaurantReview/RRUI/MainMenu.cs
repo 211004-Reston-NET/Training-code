51732840d537ef92225347cd03daf5e0fe79d0c3
Scifiler-sys <pagdilaostephen@gmail.com>
2021-11-02 16:20:23 -0400

    Remove decrepated method

--- wk4-SQL/RestaurantReview/RRUI/MainMenu.cs
+++ wk4-SQL/RestaurantReview/RRUI/MainMenu.cs
@@ -8,22 +8,17 @@ namespace RRUI
     public class MainMenu : IMenu
     {
 
-        /*
+    /*
    Since MainMenu has inherited IMenu, it will have all the methods we have created
    in IMenu.
    This is an example of Inheritance, one of the Object Oriented Pillars
-*/
+    */
         public void Menu()
         {
             Console.WriteLine("Welcome to the Main Menu!");
             Console.WriteLine("What do you want to do?");
             Console.WriteLine("[1] - Go to Restaraunt");
             Console.WriteLine("[0] - Exit");
-        }
-
-        public void ThisIsOnlyForMainMenuCFile()
-        {
-            throw new NotImplementedException();
         }
 
         public MenuType YourChoice()
